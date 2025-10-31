65afe1f91598900460e531a37a6a9f99ae1223d8
Ivan <krya.krya.krya_yes_i_am@mail.ru>
2025-10-31 15:59:01 +0300

    feat/show balance

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 'use strict';
+
+let balance = 10000;
+let isRunning = true;
+alert('Добро пожаловать!');
+while (isRunning){
+    let userPromt = prompt('Что вы хотите сделать? (показать баланс, снять средства, пополнить счёт, выйти)');
+    switch (userPromt) {
+        case 'показать баланс':
+            alert(balance);
+            break;
+        default:
+            alert('Ошибка')
+            break;
+    }
+}
